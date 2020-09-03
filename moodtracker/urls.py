11540a382aecfba6c211f30b3bc7b95533b26b62
yeonji11 <kimyj0111@naver.com>
2020-09-03 21:00:15 +0900

    [Home] Nav 감정일기 링크 연결

--- moodtracker/urls.py
+++ moodtracker/urls.py
@@ -4,7 +4,7 @@ from moodtracker import views
 
 app_name = 'moodtracker'
 urlpatterns = [    
-	path('', views.write_record),
+	path('', views.write_record, name='moodtracker_home'),
 	path('record/<int:record_id>', views.view_record, name='moodtracker_view_record'), 
 	path('write/', views.write_record, name='moodtracker_write'),
     path('create/',views.create_record, name='create_record'),
