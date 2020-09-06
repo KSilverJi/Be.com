3b675607315e77e1ae3394433486a2c32e075adc
yeonji11 <kimyj0111@naver.com>
2020-09-06 17:27:24 +0900

    [감정일기] view-mood img 추가, 디자인 수정

--- moodtracker/urls.py
+++ moodtracker/urls.py
@@ -4,7 +4,7 @@ from moodtracker import views
 
 app_name = 'moodtracker'
 urlpatterns = [    
-	path('', views.write_record),
+	path('', views.write_record, name='home'),
 	path('record/<int:record_id>', views.view_record, name='view'), 
 	path('write/', views.write_record, name='write'),
     path('create/',views.create_record, name='create'),
