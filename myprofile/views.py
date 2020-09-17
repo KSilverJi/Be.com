11c04887d7fce87c735a58b5352c819ffa3bcec4
yeonji11 <kimyj0111@naver.com>
2020-09-17 20:30:25 +0900

    [프로필] 프로필 사진 style 적용

--- myprofile/views.py
+++ myprofile/views.py
@@ -9,12 +9,12 @@ def profile_home(request):
     user = request.user
     my = MyProfile.objects.get(username=user) # 현재 사용자의 프로필 정보를 불러온다.
     class_friends = MyProfile.objects.filter(school=my.school, school_year=my.school_year, school_class=my.school_class).exclude(username=user) # 같은 학교, 학년, 반인 친구들 프로필 정보
-    # class_achievements = class_achievement(user)
+    class_achievements = 20 # 학급 활성도
     
     item={
         'my' : my,
         'class_friends' : class_friends,
-        # 'class_achievements' : class_achievements,
+        'class_achievements' : class_achievements,
     }
     return render(request, 'myprofile/myprofile.html', item)
 
